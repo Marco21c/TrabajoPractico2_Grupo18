4470f77cc8bce378828c1a66e6b5132bf8775a78
MarcosC_18 <MarcosC_18@LAPTOP-PF4EE2SM>
2022-04-14 17:42:58 -0300

    Trabajo Practico 2

---
+++ Tp2_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/Tp2Grupo18Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Grupo18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Grupo18Application.class, args);
+	}
+
+}
---
+++ Tp2_Grupo18/src/test/java/ar/edu/unju/fi/aplicacion/Tp2Grupo18ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2Grupo18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
