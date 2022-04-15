d8da775e7553d4adb505e91604f1a739a4899555
Cuquito <Cuquito@DESKTOP-LB5SU5U>
2022-04-15 03:29:57 -0300

    se creo un metodo en la clase mes para simplificar la clase principal

---
+++ Tp2_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/Punto4/Punto4.java
@@ -0,0 +1,29 @@
+package ar.edu.unju.fi.aplicacion.Punto4;
+
+import java.util.Scanner;
+
+public class Punto4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=0;
+		boolean band=true;
+		//metodo para detectar mes ingresado
+		Mes unMes = new Mes();
+		//metodo para ingreso de datos
+		Scanner entrada = new Scanner(System.in);
+		
+		while (band==true) {
+			System.out.println("ingrese mes: ");
+			num=entrada.nextInt();
+		if (num >= 1 && num <= 12){
+			unMes.CalcularMes(num);
+		}
+		else {
+			System.out.println("no existe ese mes");
+			band=false;
+		}
+	}
+	}
+
+}
