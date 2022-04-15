a60f25110f3e51a69bc67a845b42c85412ce57ed
ariel <ariel@DESKTOP-MNIDRE2>
2022-04-15 20:29:09 -0300

    ejercicio 9 resuelto con el bucle for contando 20 en 20

---
+++ Tp2_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/Punto9/Punto9.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.aplicacion.Punto9;
+
+public class Punto9 {
+
+	public static void main(String[] args) {
+		// for que recorre 20 en 20 
+		for (int i=320;i>159;i=i-20) {
+			System.out.println(i);
+		}
+	}
+
+}
