d868635784b29121b7e77719e1de2a33ff1208d6
MarcosC_18 <MarcosC_18@LAPTOP-PF4EE2SM>
2022-04-15 16:26:40 -0300

    Punto8 Multiplos de 4

---
+++ Tp2_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/Punto8/Punto8.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.aplicacion.Punto8;
+
+public class Punto8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a=0,b;
+    System.out.println("Multiplos de 4 menores o iguales a 100");
+	do{
+	a++;	
+	b= a *4;	
+	System.out.println(b);	
+	}while(b< 100);
+	
+	}
+
+}
