4cc4cae15058d45219bc4fb07af08920321ec0f4
ariel <ariel@DESKTOP-MNIDRE2>
2022-04-15 21:03:09 -0300

    punto 10 realizado, se utilizo buble while para mostrar numeros de 320 a 160

---
+++ Tp2_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/Punto10/Punto10.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.aplicacion.Punto10;
+
+public class Punto10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int acum = 320;
+		while (acum != 140) {
+			System.out.println(acum);
+			acum = acum - 20;
+		}
+	}
+
+}
