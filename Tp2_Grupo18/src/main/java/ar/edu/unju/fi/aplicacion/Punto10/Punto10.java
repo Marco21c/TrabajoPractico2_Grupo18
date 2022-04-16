2c6c5e232da8c49e3bee501087655d624392b4b3
ariel <ariel@DESKTOP-MNIDRE2>
2022-04-15 21:03:24 -0300

    realizado

--- Tp2_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/Punto10/Punto10.java
+++ Tp2_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/Punto10/Punto10.java
@@ -3,7 +3,7 @@ package ar.edu.unju.fi.aplicacion.Punto10;
 public class Punto10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// muestra numeros del 320 al 160 contando 20 en 20
 		int acum = 320;
 		while (acum != 140) {
 			System.out.println(acum);
