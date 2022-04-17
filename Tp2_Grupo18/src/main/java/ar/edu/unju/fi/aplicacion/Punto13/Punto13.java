6f6b65593c81d4edb642aaff67938aa5d5e58e80
ariel <ariel@DESKTOP-MNIDRE2>
2022-04-17 14:48:32 -0300

    se agrega comentarios

--- Tp2_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/Punto13/Punto13.java
+++ Tp2_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/Punto13/Punto13.java
@@ -3,13 +3,15 @@ package ar.edu.unju.fi.aplicacion.Punto13;
 import java.time.LocalDate;
 import java.util.Scanner;
 
+//no es necesario importar Empleado ya que esta dentro del mismo paquete
 public class Punto13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 
 		double horastrabajadas= 0.0;
 		double sueldot= 0.0;
 		LocalDate fechaIngreso = LocalDate.of(2022, 11, 10);
+		//constructor por defecto
 		Empleado unEmpleado = new Empleado("pepe", fechaIngreso, "L1305", "punto13_tp2@grupo18.com", sueldot, horastrabajadas);
 		Scanner dato =new Scanner(System.in);
 		
